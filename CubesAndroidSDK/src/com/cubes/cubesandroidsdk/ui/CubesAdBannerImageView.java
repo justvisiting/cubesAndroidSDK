afa1c1ad515a942d9ef5730a7872fbfeadb82523
souissi Haythem <suissi.haythem@gmail.com>
2013-12-30 12:39:07 -0800

    add CubesAndroidSDK

---
+++ CubesAndroidSDK/src/com/cubes/cubesandroidsdk/ui/CubesAdBannerImageView.java
@@ -0,0 +1,22 @@
+package com.cubes.cubesandroidsdk.ui;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+// TODO: override Image view in correct sizes , gravity and properties...
+public class CubesAdBannerImageView extends ImageView{
+
+	public CubesAdBannerImageView(Context context) {
+		super(context);
+	}
+	
+	public CubesAdBannerImageView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	public CubesAdBannerImageView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+	}
+
+}
---
+++ CubesAndroidSDK/src/com/cubes/cubesandroidsdk/ui/CubesAdInterstitialImageView.java
@@ -0,0 +1,22 @@
+package com.cubes.cubesandroidsdk.ui;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+//TODO: override Image view in correct sizes , gravity and properties...
+public class CubesAdInterstitialImageView extends ImageView{
+
+	public CubesAdInterstitialImageView(Context context) {
+		super(context);
+	}
+	
+	public CubesAdInterstitialImageView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	public CubesAdInterstitialImageView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+	}
+
+}
