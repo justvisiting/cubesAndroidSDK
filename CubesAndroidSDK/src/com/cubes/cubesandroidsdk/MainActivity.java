385a4091c1a0eb751994a30bea387e774e27143b
Makarenko Sergey <makarenko.s@dbbest.com>
2014-01-30 17:44:06 +0200

    Added activity (for debug purposes)

---
+++ CubesAndroidSDK/src/com/cubes/cubesandroidsdk/MainActivity.java
@@ -0,0 +1,31 @@
+package com.cubes.cubesandroidsdk;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+	
+	
+	
+	@Override
+	public void onCreate(Bundle state) {
+		super.onCreate(state);
+		setContentView(R.layout.cubes_activity);
+	}
+	
+	@Override
+	public void onResume() {
+		super.onResume();
+	}
+	
+	@Override
+	public void onPause() {
+		super.onPause();
+	}
+	
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+	}
+
+}
