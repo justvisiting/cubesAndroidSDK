ba2fe0616e48aa688f094805f2b0960866590a68
Makarenko Sergey <makarenko.s@dbbest.com>
2014-03-11 13:05:33 +0200

    Enabled full-screen closing timer

--- CubesAndroidSDK/src/com/cubes/cubesandroidsdk/smartextensions/controls/AdsControlExtension.java
+++ CubesAndroidSDK/src/com/cubes/cubesandroidsdk/smartextensions/controls/AdsControlExtension.java
@@ -300,7 +300,7 @@ public class AdsControlExtension extends ControlExtension implements
 		fullScreenControl.showInstance(instance);
 		fullScreenControl.onStart();
 		fullScreenControl.onResume();
-//		fullScreenCloseTimer.start();
+		fullScreenCloseTimer.start();
 	}
 
 	@Override
